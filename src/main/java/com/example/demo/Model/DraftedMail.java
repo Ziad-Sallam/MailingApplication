542fa5f44d66c6e15e833418e5b4b40acbddf917
Ziad-Sallam <ziadsallam1234@gmail.com>
2024-12-13 15:52:58 +0200

    Sorry my fault :)

---
+++ src/main/java/com/example/demo/Model/DraftedMail.java
@@ -0,0 +1,13 @@
+package com.example.demo.Model;
+
+public class DraftedMail extends Mail{
+    public String temp;
+
+    public String getTemp(){
+        return temp;
+    }
+    public void setTemp(String temp){
+        this.temp = temp;
+    }
+
+}
